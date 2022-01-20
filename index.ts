8a59f4a0f7983ea3f6560bb093d791694d94e205
Gustavo Alvarez <gustavoalvarez@gcflatino.org>
2022-01-20 12:14:22 -0500

    feat: initial server

---
+++ index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Well done guy!");
+});
+app.listen(3000, () => {
+  console.log("The application is listening on port 3000!");
+});

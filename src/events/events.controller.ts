be857fd273c2db95b1c4d5d48d5901584c9dcf36
JuanPalta <juanpalta24@hotmail.com>
2022-01-21 17:21:52 -0500

    feat: remove-post

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -4,30 +4,9 @@ let events = [{ id: 1, idEvent: 1, type: "modify" }];
 
 const eventRouter = Router();
 
-eventRouter.get("/", async (request: Request, response: Response) => {
+eventRouter.get("/todos", async (request: Request, response: Response) => {
   try {
     response.status(200).send(events);
-  } catch (error) {
-    console.error(error);
-    response
-      .status(500)
-      .send({ message: "Server error, contact to the admin", error });
-  }
-});
-
-eventRouter.post("/", async (request: Request, response: Response) => {
-  try {
-    if (!request.body || !request.body.title) {
-      response.status(404).send({ message: "El todo necesita un t�tulo" });
-    } else {
-      const newTodo = {
-        id: events.length + 1,
-        idEvent: request.body.idEvent,
-        type: request.body.type,
-      };
-      events.push(newTodo);
-      response.status(200).send(newTodo);
-    }
   } catch (error) {
     console.error(error);
     response

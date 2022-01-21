ffca79c93a142626a8333e149d420e73983aff43
Gustavo Alvarez <gustavoalvarez@gcflatino.org>
2022-01-21 18:27:33 -0500

    feat: agrego tareas

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -12,8 +12,9 @@ todoRouter.get("/", async (request: Request, response: Response) => {
   try {
     response.status(200).send([
       { id: 1, title: "Ir a vacunarme", complete: false },
-      { id: 1, title: "Terminar la tesis", complete: false },
-      { id: 1, title: "Estudiar GIT", complete: true },
+      { id: 2, title: "Terminar la tesis", complete: false },
+      { id: 3, title: "Estudiar GIT", complete: true },
+      { id: 4, title: "Estudiar GitHubActions", complete: true },
     ]);
   } catch (error) {
     console.error(error);
@@ -42,24 +43,24 @@ todoRouter.put("/:idTodo", async (request: Request, response: Response) => {
 });
 
 todoRouter.post("/", async (request: Request, response: Response) => {
-    try {
-        if (!request.body || !request.body.title) {
-            response.status(404).send({ message: "El todo necesita un t�tulo" });
-        } else {
-            const newTodo = {
-                id: todos.length + 1,
-                title: request.body.title,
-                complete: false,
-            };
-            todos.push(newTodo);
-            response.status(200).send(newTodo);
-        }
-    } catch (error) {
-        console.error(error);
-        response
-            .status(500)
-            .send({ message: "Server error, contact to the admin", error });
+  try {
+    if (!request.body || !request.body.title) {
+      response.status(404).send({ message: "El todo necesita un t�tulo" });
+    } else {
+      const newTodo = {
+        id: todos.length + 1,
+        title: request.body.title,
+        complete: false,
+      };
+      todos.push(newTodo);
+      response.status(200).send(newTodo);
     }
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .send({ message: "Server error, contact to the admin", error });
+  }
 });
 
 export default todoRouter;
